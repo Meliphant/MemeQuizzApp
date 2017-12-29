ca0e9e13b3fb45abf9c2aaf165c62ce2b7c26459
Meliphant <op.personal.analytics@gmail.com>
2017-12-30 01:16:51 +0300

    Added the check method on a clicking exit button.

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.quizzapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,5 +40,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
         }
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder exit = new AlertDialog.Builder(MainActivity.this);
+        exit
+                .setMessage(R.string.exitApp)
+                .setNegativeButton(R.string.yes,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                exit();
+                            }
+                        })
+                .setPositiveButton(R.string.no,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                return;
+                            }
+                        });
+
+        AlertDialog alertDialog_exit = exit.create();
+        alertDialog_exit.show();
+        Button p = alertDialog_exit.getButton(DialogInterface.BUTTON_POSITIVE);
+        p.setTextColor(Color.BLACK);
+        Button n = alertDialog_exit.getButton(DialogInterface.BUTTON_NEGATIVE);
+        n.setTextColor(Color.BLACK);
+    }
+
+    public void exit() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }
